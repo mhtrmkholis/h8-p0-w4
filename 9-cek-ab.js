51aec2f63a0170738f30249b0b4723bc5183c5a2
mhtrmkholis <46910884+mhtrmkholis@users.noreply.github.com>
2019-02-16 07:33:20 +0700

    add 9-cek-ab.js

---
+++ 9-cek-ab.js
@@ -0,0 +1,26 @@
+function checkAB(str) {
+    var posisiA = 0;
+    var posisiB = 0;
+  
+    for (i = 0; i < str.length; i++) {
+        if (str[i] === "a") {
+          posisiA = i+1
+        }
+        if (str[i] === "b") {
+          posisiB = i+1
+        }
+      }
+    if (posisiA - posisiB === 4 || posisiB - posisiA === 4) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  
+  // TEST CASES
+  console.log(checkAB('lane borrowed')); // true
+  console.log(checkAB('i am sick')); // false
+  console.log(checkAB('you are boring')); // true
+  console.log(checkAB('barbarian')); // true
+  console.log(checkAB('bacon and meat')); // false
